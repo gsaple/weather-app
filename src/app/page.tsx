da685548aa7018a996a2f02ed43f0b33f681fbf4
gsaple <yymelau@gmail.com>
2023-12-15 11:43:50 +1100

    adjust grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import BackgroundImage from "@/components/shared-ui/BackgroundImage";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center mx-auto bg-gradient-to-br from-indigo-400 to-indigo-500 relative min-w-[375px] max-w-[1280px] h-screen min-h-[46.125rem]">
+    <div className="flex flex-col justify-center mx-auto bg-gradient-to-br from-indigo-400 to-indigo-500 relative min-w-[375px] max-w-[1440px] h-screen min-h-[46.125rem]">
       <BackgroundImage
         className="absolute bottom-0 right-0 w-[14.25rem]"
         src="/backgrounds/app.png"
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -6,17 +6,17 @@ import OtherCities from "./other-cities/OtherCities";
 
 const WeatherCard: FC = () => {
   return (
-    <div className="container mx-auto bg-slate-50/90 z-10 rounded-[3.0rem] w-4/5 h-3/4 min-h-[43.125rem] max-h-[46.875rem] grid grid-cols-6 grid-rows-6 ">
-      <div className="row-span-6 col-span-2 my-7 ml-7">
+    <div className="container mx-auto bg-slate-50/90 z-10 rounded-[3.0rem] w-4/5 h-3/4 min-h-[43.125rem] max-h-[46.875rem] grid grid-cols-16 grid-rows-6 ">
+      <div className="row-span-6 col-span-5 my-7 ml-7">
         <CurrentCity />
       </div>
-      <div className="row-span-3 col-span-4 my-7 mx-9">
+      <div className="row-span-3 col-span-11 my-7 mx-9">
         <WeatherForecast />
       </div>
-      <div className="row-span-1 col-span-4 m-6 border-8">
+      <div className="row-span-1 col-span-11 m-6 border-8">
         <SearchBar />
       </div>
-      <div className="flex row-span-2 col-span-4 m-6 border-8">
+      <div className="flex row-span-2 col-span-11 m-6 border-8">
         <OtherCities />
       </div>
     </div>
